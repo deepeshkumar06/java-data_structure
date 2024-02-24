7e1bb0e9163577a1ff30bd84633beb4b3f188824
Deepesh Kumar <121308316+deepeshkumar06@users.noreply.github.com>
2024-02-24 14:13:13 +0530

    Update SumofLeafs.java

--- SumofLeafs.java
+++ SumofLeafs.java
@@ -1,4 +1,11 @@
 import java.util.*;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        this.left = null;
         this.right = null;
     }
 }
